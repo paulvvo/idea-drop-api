eed3628486d06856b24678dcbb971b4b24360033
paulvvo <paulvo@live.ca>
2018-11-05 23:43:29 -0500

    added body parser package and body parser middleware and added temporary dummy fake array database

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 
+var database = [
+	{
+		name: "jack",
+		age: 12,
+		height:5,
+		color:"blue"
+	},
+	{
+		name: "paul",
+		age: 54,
+		height:8,
+		color:"black"
+	},
+	{
+		name: "terry",
+		age: 89,
+		height:5,
+		color:"yelllow"
+	},
+	{
+		name: "berry",
+		age: 9,
+		height:5,
+		color:"pink"
+	}
+]
 app.get("/", (req,res)=>{
 	res.send("hello world");
 });
 
+app.post("/login", (req,res)=>{
 
+})
 app.listen(3001, function(){
 		console.log("listening");
 })
