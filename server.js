403275150d5f17ae12c81d60599574733f4916ea
paulvvo <paulvo@live.ca>
2018-11-06 16:45:13 -0500

    added cors to server so the front end can communicate to the backend

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require("express");
+var cors = require("cors");
 var bodyParser = require("body-parser");
 
 var app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
